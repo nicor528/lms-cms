51e5f355203bc031ba539cb6998a56d879fa64cc
nicor528 <nicolasriquelme528@gmail.com>
2023-10-19 11:59:19 -0300

    make it better

--- config/server.js
+++ config/server.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['4023da9d37973c30ea6c9ed531e59c9602151ec0607166da2181396b15b8035babd779d30960abe83631f5b3084023aea860c1ab40696dc575ae6555ecddf96ab21200822d472da696ab6d7373c59cea123249fdaccf44f3c28dce94442f0cbb422537a7a228262df10a7cf3b703b9220ba4c7477017f5772247bf70cb0a3f63']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
